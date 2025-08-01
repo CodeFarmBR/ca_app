35bbf42fdde6142ebbc69f7448685f5f79055ecd
Dudobs <dudobslol@gmail.com>
2025-08-01 12:57:45 -0300

    feat: atualiza tela de detalhes do cliente com informações do cliente no header da tela

--- src/app/cliente/[id].tsx
+++ src/app/cliente/[id].tsx
@@ -1,9 +1,11 @@
 import { useLocalSearchParams } from "expo-router"
-import { Text } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 import Header from "@/components/header"
 import { ProtectedRoute } from "@/components/protected-route"
+import { colors } from "@/themes/colors"
 import { globalStyles } from "@/themes/global-styles"
+import { typography } from "@/themes/typography"
 
 export default function ClienteDetalhesScreen() {
 	const { id } = useLocalSearchParams<{ id: string }>()
@@ -11,10 +13,31 @@ export default function ClienteDetalhesScreen() {
 	return (
 		<ProtectedRoute>
 			<SafeAreaView style={globalStyles.screenContainer}>
-				<Header backToLastPageIcon />
+				<View style={styles.headerContainer}>
+					<Header backToLastPageIcon />
 
-				<Text>Cliente com o id: {id}</Text>
+					<View style={styles.consultorInfo}>
+						<Text style={typography.headingSmBold}>Consultor</Text>
+						<Text style={[typography.bodyMd, { color: colors.gray500 }]}>
+							Empresa
+						</Text>
+					</View>
+					<Text>Cliente com o id: {id}</Text>
+				</View>
 			</SafeAreaView>
 		</ProtectedRoute>
 	)
 }
+
+const styles = StyleSheet.create({
+	headerContainer: {
+		gap: 20,
+	},
+	consultorInfo: {
+		paddingVertical: 8,
+		paddingHorizontal: 12,
+		borderWidth: 1,
+		borderColor: colors.gray50,
+		borderRadius: 4,
+	},
+})

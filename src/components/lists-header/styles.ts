f50da1919df77f7132985aee1602d8d6b504488d
Dudobs <dudobslol@gmail.com>
2025-08-05 12:18:34 -0300

    feat: implementa componente header para listas, que recebe uma função do tipo onAddPress

---
+++ src/components/lists-header/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native"
+import { colors } from "@/themes/colors"
+
+export const styles = StyleSheet.create({
+	headerList: {
+		width: "100%",
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+	titleHeaderList: {
+		color: colors.gray900,
+	},
+})

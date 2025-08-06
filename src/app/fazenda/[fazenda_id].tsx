e7d6a4e764fec4bc1d0f192cfc296f48b9fb1857
Dudobs <dudobslol@gmail.com>
2025-08-06 18:31:04 -0300

    refactor: organiza imports fora do lugar e linhas de código duplicadas

--- src/app/fazenda/[fazenda_id].tsx
+++ src/app/fazenda/[fazenda_id].tsx
@@ -1,10 +1,7 @@
+import { useLocalSearchParams } from "expo-router"
 import { Text } from "react-native"
 
 export default function FazendaDetailsScreen() {
-import { useLocalSearchParams } from "expo-router";
-import { Text } from "react-native"
-
-export default function FazendaDetailsScreen() {
-	const { fazenda_id } = useLocalSearchParams();
+	const { fazenda_id } = useLocalSearchParams()
 	return <Text>Fazenda com o id: {fazenda_id}</Text>
 }
